f4aeca739366f771dc63464daf99f2ca0af29b30
rmelchorv <rmelchorv@gmail.com>
2022-11-03 21:41:41 -0600

    Day-2 (revisited)

--- cypress/e2e/books.cy.js
+++ cypress/e2e/books.cy.js
@@ -5,20 +5,20 @@ describe('Books', () => {
       .get('[data-cy=link-to-books]').click()
     //Create book
     cy.get('[href="/books/create"]').click()
-      .get('[data-cy=input-book-title]').type('Book-Cypress')
+      .get('[data-cy=input-book-title]').type('Book-By-Cypress')
       .get('[data-cy=button-submit-book]').click()
-      .get('[data-cy=book-list]').contains('Book-Cypress')
+      .get('[data-cy=book-list]').contains('Book-By-Cypress')
     //Show book
     cy.get('[data-cy^=link-to-visit-book-]').last().click()
-      .get('h1').should('contain.text', 'Book-Cypress')
-      .get('[href="/books/create"]').click()
+      .get('h1').should('contain.text', 'Book-By-Cypress')
+      .get('[href="/books"]').click()
     //Edit
     cy.get('[data-cy^=link-to-edit-book-]').last().click()
-      .get('[data-cy=input-book-title]').clear().type('Book-Cypress-Edited')
+      .get('[data-cy=input-book-title]').clear().type('Book-By-Cypress-Edited')
       .get('[data-cy=button-submit-book]').click()
-      .get('[data-cy=book-list]').contains('Book-Cypress-Edited')
+      .get('[data-cy=book-list]').contains('Book-By-Cypress-Edited')
     //Delete
     cy.get('[data-cy^=link-to-delete-book-]').last().click()
-      .get('[data-cy^=link-to-visit-book-]').last().should('not.contain.text', 'Book-Cypress-Edited')
+      .get('[data-cy^=link-to-visit-book-]').last().should('not.contain.text', 'Book-By-Cypress-Edited')
   })
 })
